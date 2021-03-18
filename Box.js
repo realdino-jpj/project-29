0544cfe7f7865692ceda2c9192234511b78e569d
realdino-jpj <78307958+realdino-jpj@users.noreply.github.com>
2021-03-17 22:45:20 -0700

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,27 @@
+class Box  {
+    constructor(x,y,width,height) {
+        var options = {
+            isStatic: false,
+            restitution:0.8,
+            friction:1,
+            density:1
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var pos =this.body.position;
+        rectMode(CENTER);
+        fill("blue");
+        rect(pos.x, pos.y, this.width, this.height);
+
+
+
+      }
+    
+  
+  
+  }
+  
